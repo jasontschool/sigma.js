f6468b8302462d78bf0007c3c4ce7efc9dbd0212
jteoh <jteoh@linkedin.com>
2015-04-27 03:05:28 -0700

    Tentative hover + label alignment setting. Need to clean up and/or submit pull req

--- src/renderers/canvas/sigma.canvas.hovers.def.js
+++ src/renderers/canvas/sigma.canvas.hovers.def.js
@@ -14,18 +14,32 @@
    * @param  {CanvasRenderingContext2D} context  The canvas context.
    * @param  {configurable}             settings The settings function.
    */
+   //TODO temp override of def.
   sigma.canvas.hovers.def = function(node, context, settings) {
     var x,
         y,
         w,
         h,
         e,
+        v,
         fontStyle = settings('hoverFontStyle') || settings('fontStyle'),
         prefix = settings('prefix') || '',
         size = node[prefix + 'size'],
         fontSize = (settings('labelSize') === 'fixed') ?
           settings('defaultLabelSize') :
-          settings('labelSizeRatio') * size;
+          settings('labelSizeRatio') * size,
+        labelWidth = 0,
+        labelPlacementX,
+        labelPlacementY,
+        alignment;
+
+
+    if (node['labelAlignment'] === undefined){ 
+      alignment = settings('defaultLabelAlignment');
+    } else {
+      alignment = node['labelAlignment'];
+    }
+
 
     // Label background:
     context.font = (fontStyle ? fontStyle + ' ' : '') +
@@ -43,22 +57,137 @@
       context.shadowColor = settings('labelHoverShadowColor');
     }
 
+    //TODO - going to need to figure out the border drawing...
+    // need to understand how context drawing works...
+    //TODO figure out where to inject the hovers type
+    labelWidth = context.measureText(node.label).width;
+
+    if (alignment === 'inside') {
+      if (labelWidth > size * 2) {
+        //too large to fit in node, use default value
+        alignment = settings('defaultLabelAlignment');
+        if (alignment === 'inside') {
+          //don't use the default
+          alignment = undefined;
+        }
+      } else {
+        alignment = 'center';
+      } 
+    }
+
+    labelPlacementY = Math.round(node[prefix + 'y'] + fontSize / 3);
+    switch (alignment) {
+      case 'center':
+        labelPlacementX = Math.round(node[prefix + 'x'] - labelWidth / 2 );
+        break;
+      case 'left':
+        labelPlacementX = Math.round(node[prefix + 'x'] - size - labelWidth - 3 );
+        break;
+      case 'top':
+        labelPlacementX = Math.round(node[prefix + 'x'] - labelWidth / 2 );
+        labelPlacementY = labelPlacementY - size - fontSize;
+        break;
+      case 'bottom':
+        labelPlacementX = Math.round(node[prefix + 'x'] - labelWidth / 2 );
+        labelPlacementY = labelPlacementY + size + fontSize;
+        break;
+      case 'right':
+      default:
+        // Default is aligned 'right'
+        labelPlacementX = Math.round(node[prefix + 'x'] + size + 3);
+        break;
+    }
+    //END hack edit
+
+
     if (node.label && typeof node.label === 'string') {
       x = Math.round(node[prefix + 'x'] - fontSize / 2 - 2);
       y = Math.round(node[prefix + 'y'] - fontSize / 2 - 2);
       w = Math.round(
-        context.measureText(node.label).width + fontSize / 2 + size + 7
+        context.measureText(node.label).width  + size + 5
       );
       h = Math.round(fontSize + 4);
       e = Math.round(fontSize / 2 + 2);
-
-      context.moveTo(x, y + e);
-      context.arcTo(x, y, x + e, y, e);
-      context.lineTo(x + w, y);
-      context.lineTo(x + w, y + h);
-      context.lineTo(x + e, y + h);
-      context.arcTo(x, y + h, x, y + h - e, e);
-      context.lineTo(x, y + e);
+      v = size - 4;
+      //the hover code is hardcoded to default node type :(
+      switch(alignment) {
+        case 'center':
+          if (labelWidth <= size * 2) {
+            //completely inside:
+            //this should hover larger than the outline
+            context.arc(x + e,y + e, size + settings('borderSize') + 2, 0, Math.PI * 2);
+          } else {
+            //top left corner of rect
+            context.moveTo(x + e - w / 2, y + e - h / 2);
+            //top of rect
+            context.lineTo(x + e + w / 2, y + e - h / 2);
+            //right side
+            context.lineTo(x + e + w / 2, y + e + h / 2);
+            //bottom
+            context.lineTo(x + e - w / 2, y + e + h / 2);
+            //left
+            context.lineTo(x + e - w / 2, y + e - h / 2);
+          }
+          break;
+        case 'left':
+          //right half of circle
+          context.arc(x + e,y + e, e, Math.PI * 0.5, Math.PI * 1.5, true);
+          //top of semi-circle
+          context.moveTo(x+e, y);
+          //top of rect
+          context.lineTo(x + e - w, y);
+          //right side of rect
+          context.lineTo(x + e - w, y + h);
+          //bottom of rect
+          context.lineTo(x + e, y + h);
+          break;
+        case 'top': 
+          //bottom half of circle
+          context.arc(x + e,y + e, e, 0, Math.PI);
+          //left side of semi-circle
+          context.moveTo(x, y+e);
+          //botLeft of rectangle - refactor size w/ fontSize
+          context.lineTo(x + e - w / 2, y - v);
+          //left side
+          context.lineTo(x + e - w / 2, y - v - h);
+          // top
+          context.lineTo(x + e + w / 2, y - v - h);
+          // right side
+          context.lineTo(x + e + w / 2, y - v);
+          //back to semi-circle, right side
+          context.lineTo(x + 2 * e, y + e);
+          break;
+        case 'bottom':
+          //top half of circle
+          context.arc(x + e,y + e, e, 0, Math.PI, true);
+          //left side of semi-circle
+          context.moveTo(x, y+e);
+          //topLeft of rectangle - refactor size w/ fontSize
+          context.lineTo(x + e - w / 2, y + 2*e + v);
+          //left side
+          context.lineTo(x + e - w / 2, y + 2*e + v + h);
+          // top
+          context.lineTo(x + e + w / 2, y + 2*e + v + h);
+          // right side
+          context.lineTo(x + e + w / 2, y + 2*e + v);
+          //back to semi-circle, right side
+          context.lineTo(x + 2 * e, y + e);
+          break;
+        case 'right':
+        default:
+          //left half of circle
+          context.arc(x + e,y + e, e, Math.PI * 0.5, Math.PI * 1.5);
+          //top of semi-circle
+          context.moveTo(x + e, y);
+          //top of rect
+          context.lineTo(x + e + w, y);
+          //right side of rect
+          context.lineTo(x + e + w, y + h);
+          //bottom of rect
+          context.lineTo(x + e, y + h);
+          break;
+      }
+            
 
       context.closePath();
       context.fill();
@@ -68,7 +197,7 @@
       context.shadowBlur = 0;
     }
 
-    // Node border:
+    // Node border: note this is hardcoded for the default circle node
     if (settings('borderSize') > 0) {
       context.beginPath();
       context.fillStyle = settings('nodeBorderColor') === 'node' ?
@@ -98,8 +227,8 @@
 
       context.fillText(
         node.label,
-        Math.round(node[prefix + 'x'] + size + 3),
-        Math.round(node[prefix + 'y'] + fontSize / 3)
+        labelPlacementX,
+        labelPlacementY
       );
     }
   };
--- src/renderers/canvas/sigma.canvas.labels.def.js
+++ src/renderers/canvas/sigma.canvas.labels.def.js
@@ -1,3 +1,4 @@
+//heavily modified from: https://github.com/jacomyal/sigma.js/pull/291/files
 ;(function(undefined) {
   'use strict';
 
@@ -8,7 +9,7 @@
   sigma.utils.pkg('sigma.canvas.labels');
 
   /**
-   * This label renderer will just display the label on the right of the node.
+   * This label renderer will display the label according to labelAlignment.
    *
    * @param  {object}                   node     The node object.
    * @param  {CanvasRenderingContext2D} context  The canvas context.
@@ -17,13 +18,24 @@
   sigma.canvas.labels.def = function(node, context, settings) {
     var fontSize,
         prefix = settings('prefix') || '',
-        size = node[prefix + 'size'];
+        size = node[prefix + 'size'],
+        labelWidth = 0,
+        labelPlacementX,
+        labelPlacementY,
+        alignment;
 
     if (size < settings('labelThreshold'))
       return;
 
     if (!node.label || typeof node.label !== 'string')
       return;
+
+    //jteoh - modified to per-node setting
+    if (node['labelAlignment'] === undefined){ 
+      alignment = settings('defaultLabelAlignment');
+    } else {
+      alignment = node['labelAlignment'];
+    }
 
     fontSize = (settings('labelSize') === 'fixed') ?
       settings('defaultLabelSize') :
@@ -35,10 +47,48 @@
       (node.color || settings('defaultNodeColor')) :
       settings('defaultLabelColor');
 
+    labelWidth = context.measureText(node.label).width;
+
+    if (alignment === 'inside') {
+      if (labelWidth > size * 2) {
+        //too large to fit in node, use default value
+        alignment = settings('defaultLabelAlignment');
+        if (alignment === 'inside') {
+          //don't use the default
+          alignment = undefined;
+        }
+      } else {
+        alignment = 'center';
+      } 
+    }
+
+    labelPlacementY = Math.round(node[prefix + 'y'] + fontSize / 3);
+    switch (alignment) {
+      case 'center':
+        labelPlacementX = Math.round(node[prefix + 'x'] - labelWidth / 2 );
+        break;
+      case 'left':
+        labelPlacementX = Math.round(node[prefix + 'x'] - size - labelWidth - 3 );
+        break;
+      case 'top':
+        labelPlacementX = Math.round(node[prefix + 'x'] - labelWidth / 2 );
+        labelPlacementY = labelPlacementY - size - fontSize;
+        break;
+      case 'bottom':
+        labelPlacementX = Math.round(node[prefix + 'x'] - labelWidth / 2 );
+        labelPlacementY = labelPlacementY + size + fontSize;
+        break;
+      case 'right':
+      default:
+        // Default is aligned 'right'
+        labelPlacementX = Math.round(node[prefix + 'x'] + size + 3);
+        break;
+    }
+
     context.fillText(
       node.label,
-      Math.round(node[prefix + 'x'] + size + 3),
-      Math.round(node[prefix + 'y'] + fontSize / 3)
+      labelPlacementX,
+      labelPlacementY
     );
   };
 }).call(this);
